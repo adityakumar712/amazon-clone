5265dce8fc8192f6fb4d4426cb61e49881e5ead2
Aditya <aditya980120@gmail.com>
2026-05-21 03:14:32 +0530

    save cart into localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart =[{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart = [{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity  :2
 },
@@ -9,6 +12,13 @@ export let cart =[{
 }
 
 ];
+}
+
+
+
+function saveToStorage(){
+    localStorage.setItem('cart' , JSON.stringify(cart));
+}
 
 //function for adding the products to the cart.
 export function addToCart(productId, selectorQunatityValue){
@@ -29,6 +39,7 @@ export function addToCart(productId, selectorQunatityValue){
             quantity : selectorQunatityValue,
         });
         }
+        saveToStorage();
 }
 
 
@@ -43,5 +54,6 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 }
 
