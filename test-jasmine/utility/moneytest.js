8814b0881d01c01eb5f457dcd7004bc7104f9acf
Aditya <aditya980120@gmail.com>
2026-05-25 17:27:32 +0530

    testing the addToCart using jasmine

--- test-jasmine/utility/moneytest.js
+++ test-jasmine/utility/moneytest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../javascript/utility/money.js';
+import {formatCurrency} from '../../javascript/utility/money.js';
 
 describe('test suite: formatCurrency' , ()=>{
     it('converts cents into dollars' , ()=>{
