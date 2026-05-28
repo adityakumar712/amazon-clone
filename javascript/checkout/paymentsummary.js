414f1c248f6dad948a9a1dc28771791106aa7973
Aditya <aditya980120@gmail.com>
2026-05-28 17:17:30 +0530

    create order using backend and make order button interactive

--- javascript/checkout/paymentsummary.js
+++ javascript/checkout/paymentsummary.js
@@ -2,6 +2,7 @@ import {cart , calculateCartQuantity} from '../../data/cart.js';
 import{getProductById} from '../../data/products.js';
 import {getDeliveryOptionById} from '../../data/deliveryOption.js';
 import{formatCurrency} from '../utility/money.js';
+import { addOrder } from '../../data/order.js';
 
 export function renderPaymentSummary(){
     let productTotalCents = 0;
@@ -62,13 +63,40 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button js-place-order-button button-primary">
             Place your order
           </button>
     
     `;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
+
+    document.querySelector('.js-place-order-button').addEventListener
+    ('click' , async()=>{
+
+      try{
+        const response = await fetch('https://supersimplebackend.dev/orders' ,{
+        method:'POST',
+        headers:{
+          'content-type' : 'application/json'
+        },
+        body:JSON.stringify({
+          cart:cart
+        }) 
+      })
+
+        const order =  await response.json();  //this is also promise(response).
+        addOrder(order);
+
+         //now we check on the console orders stored on localstorage(ordersummary).
+
+      }catch(error){
+          console.log('unexpected Error . try again later');
+      }
+      
+      window.location.href='orders.html';
+    
+    });
 
 }
 
